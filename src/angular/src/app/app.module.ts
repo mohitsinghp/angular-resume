75ba93ee998dbbaf5b27e88602f9657ea643f144
Mohit Singh Parihar <mohitsinghparihar1511@gmail.com>
2021-06-08 15:23:35 +0530

    changes in the JSON file

--- src/angular/src/app/app.module.ts
+++ src/angular/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ProjectsComponent } from './projects/projects.component';
 import { ExperienceComponent } from './experience/experience.component';
 import { ObjectiveComponent } from './objective/objective.component';
 import { SummaryComponent } from './summary/summary.component';
+import { CertificationComponent } from './certification/certification.component';
 
 @NgModule({
   declarations: [
@@ -27,7 +28,8 @@ import { SummaryComponent } from './summary/summary.component';
     ProjectsComponent,
     ExperienceComponent,
     ObjectiveComponent,
-    SummaryComponent
+    SummaryComponent,
+    CertificationComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/angular/src/app/certification/certification.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-certification',
+  templateUrl: './certification.component.html',
+  styleUrls: ['./certification.component.scss']
+})
+export class CertificationComponent implements OnInit {
+
+  constructor() { }
+
+  @Input() certification: [];
+
+  ngOnInit(): void {
+  }
+
+}
